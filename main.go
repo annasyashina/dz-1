c83ad2ed0114938f99447c9f4e70076e6a9e1b9a
sowa <sowa84@bk.ru>
2025-10-13 11:43:59 +0400

    Решение ДЗ строки и функции

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	var sourceCurrency string
+	var wantConvet string
+	var convertibleCurrency string
+	sourceCurrency, wantConvet, convertibleCurrency = getUserInput()
 	const USD_EUR = 0.8549
 	const USD_RUB = 81.9
 	//EUR_USD := 1 / USD_EUR
@@ -10,4 +14,21 @@ func main() {
 	//EUR_RUB := EUR_USD / RUB_USD
 	EUR_RUB := USD_RUB / USD_EUR
 	fmt.Println(EUR_RUB)
+}
+
+func getUserInput() (string, string, string) {
+	var sourceCurrency string
+	var wantConvet string
+	var convertibleCurrency string
+	fmt.Println("Введите исходную валюту:")
+	fmt.Scan(&sourceCurrency)
+	fmt.Println("Введите, что хотим конвертировать:")
+	fmt.Println(&wantConvet)
+	fmt.Println("Введите конвертируемую валюту:")
+	fmt.Scan(&convertibleCurrency)
+	return sourceCurrency, wantConvet, convertibleCurrency
+}
+
+func calculation(float64, string, string) {
+
 }
