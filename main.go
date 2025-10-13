7f16232b38a159bb85e6cadeb4b36695d456638a
sowa <sowa84@bk.ru>
2025-10-13 16:13:13 +0400

    Решение ДЗ строки и функции удаление неиспользуемых переменных

--- main.go
+++ main.go
@@ -4,11 +4,11 @@ import "fmt"
 
 func main() {
 
-	var sourceCurrency string
+	/*var sourceCurrency string
 	var wantConvet string
 	var convertibleCurrency string
 	sourceCurrency, wantConvet, convertibleCurrency = getUserInput()
-
+	*/
 	const USD_EUR = 0.8549
 	const USD_RUB = 81.9
 	//EUR_USD := 1 / USD_EUR
