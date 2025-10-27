aeaf9e6431435876a791d59e7b5527c6f02a981c
sowa <sowa84@bk.ru>
2025-10-27 20:04:36 +0400

    Решение ДЗ указатели на вложенные мапы

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 	m_usd["RUB"] = USD_RUB
 	m_usd["EUR"] = USD_EUR
 
-	m := map[string]convertMap{"USD": m_usd, "EUR": m_evro, "RUB": m_rub}
+	m := map[string]*convertMap{"USD": &m_usd, "EUR": &m_evro, "RUB": &m_rub}
 	result := calculation(&m, wantConvet, sourceCurrency, convertibleCurrency)
 	//fmt.Println(m[sourceCurrency][convertibleCurrency])
 	//fmt.Println(reflect.TypeOf((m[sourceCurrency][convertibleCurrency])))
@@ -91,11 +91,11 @@ func getUserInput() (string, float64, string) {
 	return sourceCurrency, number, convertibleCurrency
 }
 
-func calculation(m *map[string]convertMap, number float64, current string, convert string) float64 {
+func calculation(m *map[string]*convertMap, number float64, current string, convert string) float64 {
 	var result float64
 
-	//fmt.Println(*m[current])
-	result = (number) * ((*m)[current][convert])
+	//fmt.Println((*(*m)[current])[convert])
+	result = (number) * (*(*m)[current])[convert]
 	/*	switch current {
 		case "USD":
 			switch convert {
